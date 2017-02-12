3a261337fa1ccaf914856bd0a6f9932453b33f96
mullerjs <jeremysmuller@gmail.com>
2017-02-12 16:42:53 -0700

    first commit

---
+++ sketch.js
@@ -0,0 +1,22 @@
+//create a synth and connect it to the master output (your speakers)
+var synth = new Tone.Synth().toMaster();
+
+//play a middle 'C' for the duration of an 8th note
+synth.triggerAttackRelease("C4", "1n", 3);
+
+var count = 0;
+
+StartAudioContext(Tone.context, ".startButton").then(function() {
+		console.log("STARTED!!!");
+});
+
+function updateTime(){
+	requestAnimationFrame(updateTime);
+
+	//Tone.now returns the current AudioContext time
+	document.getElementById("text").innerHTML = "AudioContext " + Tone.now().toFixed(2);
+	// console.log(Tone.now().toFixed(2));
+
+	count++;
+}
+updateTime()
