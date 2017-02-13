c7541072b9cf364a48f79a8210983582448f0515
mullerjs <jeremysmuller@gmail.com>
2017-02-13 08:27:25 -0700

    changed button and got rid of bootstrap

--- sketch.js
+++ sketch.js
@@ -6,12 +6,10 @@ var synth = new Tone.Synth().toMaster();
 
 var count = 0;
 
-// StartAudioContext(Tone.context, '#start', function() {
-// 		console.log("STARTED!!!");
-// });
 StartAudioContext(Tone.context, '#start').then(function(){
 	//play a middle 'C' for the duration of an 8th note
 	synth.triggerAttackRelease("C4", "1n", 3);
+	console.log("STARTED!!!");
 });
 
 function updateTime(){
