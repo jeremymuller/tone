fe83264671964a7fcee2f276b94777e050d29146
mullerjs <jeremysmuller@gmail.com>
2017-02-14 09:52:49 -0700

    attached to <body>

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ var synth = new Tone.Synth().toMaster();
 var count = 0;
 
 // StartAudioContext(Tone.context, '#start');
-StartAudioContext(Tone.context);
+StartAudioContext(Tone.context, 'body');
 
 synth.triggerAttackRelease("C5", "1n", 3);
 
