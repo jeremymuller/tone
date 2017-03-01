0b6cfcfc9d28f1c20b1be7030b94dee45c35574e
mullerjs <jeremysmuller@gmail.com>
2017-03-01 16:47:55 -0700

    attack time isn't working

--- sketch.js
+++ sketch.js
@@ -1,5 +1,12 @@
 //create a synth and connect it to the master output (your speakers)
 var synth = new Tone.Synth().toMaster();
+synth.set({
+	"envelope" : {
+		"attack" : 2,
+		"release" : 2,
+		"attackCurve" : "linear",
+	}
+});
 
 //play a middle 'C' for the duration of an 8th note
 // synth.triggerAttackRelease("C4", "1n", 3);
@@ -9,10 +16,6 @@ var bgColor = Math.random() * 360; // hue
 
 // StartAudioContext(Tone.context, '#start');
 StartAudioContext(Tone.context);
-
-// synth.triggerAttackRelease("C5", 0.001, 0, 0.9);
-// synth.triggerAttack("C5", 0, 0.9);
-// synth.triggerRelease(0.5);
 
 var clock = new Tone.Clock(function(time){
 	console.log(time);
@@ -24,8 +27,8 @@ function loopDis(time) {
 	playSound();
 }
 
-var loop = new Tone.Loop(loopDis, 0.5);
-loop.start(0).stop(5);
+var loop = new Tone.Loop(loopDis, 5);
+loop.start(0).stop(30);
 
 Tone.Transport.start(2);
 
@@ -38,19 +41,13 @@ function updateTime(){
 
 	document.body.style.backgroundColor = "hsl(" + bgColor + ", 100%, 50%)";
 
-	// if ((Math.random() * 100) < 3) {
-	// 	playSound();
-	// }
-
 	bgColor += 0.1;
 	count++;
 }
 updateTime()
 
 function playSound() {
-	synth.envelope.attack = 0.1;
-	synth.envelope.release = 1;
 	synth.triggerAttack("C5", 0, 0.9);
 	synth.triggerRelease("+0.3");
-	console.log("belafonte");
+	console.log("play!");
 }
